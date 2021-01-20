dbaddeb67f46c3213d40bd1e85a9fddae544f19e
JoseDeLaVega <josephdlv@hotmail.com>
2021-01-19 20:35:59 -0600

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,14 @@
+/**
+* @author Jose De La Vega 
+* @fecha 19/01/2021
+* @proposito Interfaz
+* @Grupo Rebecca Smith-Jose De La Vega
+*/
+
+public interface Interface{
+  public boolean encenderApagar(); 
+  public boolean amfm(); // true = am; false = fm
+  public String guardar(int btn);
+  public String seleccionar(int btn);
+  public double avanzar();
+}
